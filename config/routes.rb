90e7e3b6c0c40b6003af1a5a80b089dce88ba4b5
stephen pearson <stephen.pearson@invision.com>
2011-11-25 09:23:12 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 SampleApp::Application.routes.draw do
 
-	match '/Contact', 	:to => 'pages#contact'
-	match '/about',		:to => 'pages#about'
-	match '/help',			:to => 'pages#help'
-	
-root :to => 'pages#home'
+  get "users/new"
 
-  #get "pages/home"
+	match '/signup',	:to => 'users#new'
+	match '/contact', :to => 'pages#contact'
+	match '/about',   :to => 'pages#about'
+	match '/help',    :to => 'pages#help'
 
- #get "pages/contact"
+	root :to => 'pages#home'
+
+ #get "pages/home"
+
+# get "pages/contact"
   
  #get "pages/about"
  
